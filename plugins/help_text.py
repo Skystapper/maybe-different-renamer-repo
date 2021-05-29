294b0b73b6049021e9bf701377d8681e4ffe47a7
Rajnish Singh <79822364+coderboy1952@users.noreply.github.com>
2021-05-29 16:21:42 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -30,7 +30,7 @@ def help_user(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.HELP_USER,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕️ Contact DEV ⭕️", url="https://t.me/prgofficial")]]),
+        #reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Contact", url="https://t.me/prgofficial")]]),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
@@ -87,5 +87,5 @@ async def cancel_extract(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text="Process Cancelled 🙃",
+        text="Process Cancelled successfully.",
     )

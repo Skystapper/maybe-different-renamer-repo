1d6b3ec2e5e4aee512862292db37816f196291c2
Rajnish Singh <79822364+coderboy1952@users.noreply.github.com>
2021-05-29 16:18:04 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 class script(object):
     START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support.
 Send me any Telegram file and choose appropriate option.
-Try Not to Spam me """
+Send one file at a time.
+Try Not to Spam me.
+
+/help for more details..."""
     RENAME_403_ERR = "What Are You Doing? You are Banned"
     UPGRADE_TEXT = "This Bot is Completely free to use."
     DOWNLOAD_START = "Downloading the file..."
@@ -11,7 +14,7 @@ Try Not to Spam me """
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!"
     SAVED_RECVD_DOC_FILE = "File Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = " "
+    CUSTOM_CAPTION_UL_FILE = "@Rename_Rbot"
     HELP_USER = """It's not that complicated
     
 1. Send me any Telegram File.
